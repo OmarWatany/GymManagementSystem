9837376bb131fc0ace8a18dfb8c1954f67b6611b
OmarWatany <watany6500@gmail.com>
2023-12-03 20:39:04 +0200

    added working hours method

--- src/GYM/Coach.java
+++ src/GYM/Coach.java
@@ -2,15 +2,20 @@ package GYM;
 public class Coach{
     private final String name;
     private final int age;
-    public Coach(int age,String name){
+    private int workingHours;
+    public Coach(int age,String name,int workingHours){
         this.name = name;
         this.age  = age;
+        this.workingHours = Math.min(workingHours, 10);
     }
     public void getName(){
         System.out.println(name);
     }
     public void getAge(){
         System.out.println(age);
+    }
+    public void getWorkingHours(){
+        System.out.println(workingHours);
     }
     public void listCustomers(){
     }
