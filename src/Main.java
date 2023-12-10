fda5778b8e69a6eea13a595b3fdd7ca67ff3b359
OmarTebry <omartebry@gmail.com>
2023-12-10 17:43:55 +0200

    teslam ya5ooya watany.

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         // for testing
         Date date = new Date();
         Subscription sub = new Subscription();
+        Gym gym = new Gym("Mommy Gym","3and ommak",69);
+        gym.DisplayGymInfo();
         Customer omar = new Customer(234,"omar",MALE,
                 "234 st as","012351235","omar@email.com",sub);
         omar.inBodies[0] = new InBody(date,1.5F,15F,1F,10F,0.5F);
@@ -17,6 +19,6 @@ public class Main {
         Coach ahmed = new Coach(23,"Ahmed",MALE,"23 Ahmed st",
                 "012345678901","Ahmed@email.com",9);
         ahmed.customers[0] = omar;
-        ahmed.getCustomerInBodyHistory("omar");
+       // ahmed.getCustomerInBodyHistory("omar");
     }
 }
