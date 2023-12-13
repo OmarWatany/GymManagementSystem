f39f6da044c1e8df46673823b51cf2fda70b00c5
Omar-Elhakim <omarelhakim@icloud.com>
2023-12-13 14:53:09 +0200

    made gender string for now , added readFile function that read all files into its right place in the gym , added an admin arraylist to store them and removed his name (7ases malosh lazma), added customerId into Inbody class to map it to its customer , fe 7agat tany me4 fakerha

--- src/Data/Gym.java
+++ src/Data/Gym.java
@@ -11,27 +11,32 @@ public class Gym {
     public static ArrayList<Subscription> Subscriptions = new ArrayList<>();
     public static ArrayList<Equipment> Equipments = new ArrayList<>();
     public static ArrayList<Customer> Customers = new ArrayList<>();
+    public static ArrayList<Admin> Admins = new ArrayList<>();
 
-    public Gym(String name,String address,int num){
+    public Gym(String name, String address, int num) {
         Name = name;
         Address = address;
-        PhoneNumber=num;
+        PhoneNumber = num;
     }
-    public void DisplayGymInfo(){
-        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-" );
-        System.out.println("  welcome to " +Name);
-        System.out.println("Our Address :  " +Address);
-        System.out.println("Our Phone Number :  " +PhoneNumber);
-        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-" );
+
+    public void DisplayGymInfo() {
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+        System.out.println("  welcome to " + Name);
+        System.out.println("Our Address :  " + Address);
+        System.out.println("Our Phone Number :  " + PhoneNumber);
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
     }
-    public void AddCustomer(int id,String name,Gender gender,String address,String number,String email,Subscription subscription){
-        Customers.add(new Customer(id,name,gender,address,number,email,subscription));
+
+    public void AddCustomer(int id, String name, String gender, String address, String number, String email,
+            Subscription subscription) {
+        Customers.add(new Customer(id, name, gender, address, number, email, subscription));
 
     }
+
     public void displayCoaches() {
         System.out.println("Coaches:");
         for (Coach coach : Coaches) {
-            System.out.println(coach.ID+"  "+coach.Name);
+            System.out.println(coach.ID + "  " + coach.Name);
         }
         System.out.println("  ");
     }
@@ -55,21 +60,24 @@ public class Gym {
     public void displayCustomers() {
         System.out.println("Customers:");
         for (Customer customer : Customers) {
-            System.out.println(customer.ID+"   "+customer.Name);
+            System.out.println(customer.ID + "   " + customer.Name);
         }
         System.out.println("  ");
     }
-    public static int gettotalprice(){
-        int totalprice=0;
+
+    public static int gettotalprice() {
+        int totalprice = 0;
         for (Subscription subscription : Subscriptions) {
-            //totalprice += subscription.price;
+            // totalprice += subscription.price;
         }
         return totalprice;
     }
-    public static int gettotalsalary(){
-        int totalsalary=0;
+
+    public static int gettotalsalary() {
+        int totalsalary = 0;
         for (Coach coach : Coaches) {
-            totalsalary += coach.salary;;
+            totalsalary += coach.salary;
+            ;
         }
         return totalsalary;
     }
--- src/Persons/Person.java
+++ src/Persons/Person.java
@@ -1,17 +1,18 @@
 package Persons;
+
 import Data.*;
 
-
-public abstract  class Person{
+public abstract class Person {
     public int ID;
-    public  String Name,Address,Phone_Number,Email;
-    public Gender gender;
-    public Person(int id,String name,Gender gender, String address,String phone_number,String email){
-        ID=id;
-        Name=name;
-        this.gender=gender;
-        Address=address;
-        Phone_Number=phone_number;
-        Email=email;
+    public String Name, Address, Phone_Number, Email;
+    public String gender;
+
+    public Person(int id, String name, String gender, String address, String phone_number, String email) {
+        ID = id;
+        Name = name;
+        this.gender = gender;
+        Address = address;
+        Phone_Number = phone_number;
+        Email = email;
     }
-} 
+}
